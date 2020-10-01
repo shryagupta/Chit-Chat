4a3675ba12da64f8ddf7cc1788f327d6a4c3cfc7
shryagupta <aanya9452@gmail.com>
2020-10-01 17:19:29 +0530

    user logging in

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Login from "./Components/login"
+import Main from './Components/Main'
+import {BrowserRouter as Router , Route ,Switch} from 'react-router-dom'
+import './App.css';
+
+
+
+
+function App() {
+  const [user , setuser]  = useState(null) ;
+  
+  
+  return (
+    <div className="App">
+
+      {!user ? <Login set_user={setuser} /> :
+        (<Router>
+          <Switch>
+            <Main user={user} />
+          </Switch >
+        </Router> ) }
+      
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/login.js
@@ -0,0 +1,50 @@
+import React  from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import {auth , provider} from './firebase' ;
+import '../Style/Login.css';
+
+
+function Login(props){
+
+  const signin = () =>{
+    auth.signInWithPopup(provider).then(result => props.set_user({id :result.additionalUserInfo.profile.id, name:result.additionalUserInfo.profile.name })).catch(error => alert(error.message))
+  }
+
+
+    const useStyles = makeStyles({
+        root: {
+          minWidth: 100,
+          boxShadow: 25,
+          alignContent: 100,
+        },
+        title: {
+            fontSize: 25,
+          },
+          title2: {
+            fontSize: 55,
+          },
+    });
+          const classes = useStyles();
+    return(
+        <div className = "container">
+          <div className ="card">            
+            <div className='login_header '>
+                CHIT - CHAT {'  '}<i class="far fa-comment-dots"></i>       
+            </div>
+            <div className='login-footer'>
+              <div>
+                 Your Chat Room !
+              </div><hr></hr>
+              <Button variant="contained" color="primary" href="#contained-buttons" onClick={signin}>
+              <i class="fab fa-google"></i> Login with Google
+              </Button>
+            </div>
+              
+          </div>   
+        </div>
+    );
+}
+
+export default Login;
